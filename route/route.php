7f8715bc9f027ca0361c273b2b014e86a59f893d
amosmz <amosmz@163.com>
2018-07-18 14:13:16 +0800

    route.php

--- route/route.php
+++ route/route.php
@@ -12,6 +12,7 @@ Route::rule('/', 'index'); // 首页访问路由
 Route::rule('download', 'index/download'); // 静态地址路由
 Route::rule('about', 'index/about'); // 静态地址路由
 Route::get('article/:id','index/article');
+Route::get('tag/:id','index/tag');
 return [
 
 ];
